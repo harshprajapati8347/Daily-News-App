06f8a9fb4b7a2cbf9df82e20c129abf33b5d45fe
Harsh Prajapati <64133891+harshprajapati8347@users.noreply.github.com>
2022-04-23 23:26:09 +0530

    heroku update

--- server/index.js
+++ server/index.js
@@ -19,12 +19,28 @@ const PORT = process.env.PORT || 8000;
 connection();
 
 // Heroku
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
+
+// TODO: Deployment
+
+__dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*",(req,res)=>{
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  })
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
   });
 }
+
+// TODO: Deployment
 
 app.listen(
   PORT,
